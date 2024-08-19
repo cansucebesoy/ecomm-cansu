716c7f45666fdb9529b55c6c2457621a3959f88c
cansucebesoy <cansucebesoy3@gmail.com>
2024-08-19 23:46:34 +0300

    first commit

---
+++ src/layout/Header.jsx
@@ -0,0 +1,12 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+export const Header = () => {
+  return (
+    <div>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </div>
+  );
+};
