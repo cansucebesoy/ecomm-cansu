c6321cf0f7fc521a74af99638152e5f3990479bc
cansucebesoy <cansucebesoy3@gmail.com>
2024-08-29 00:21:14 +0300

    activate router and links

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -1,13 +1,4 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { useHistory } from "react-router-dom";
-import { Button } from "@/components/ui/button";
+import { useParams } from "react-router-dom";
 
 const cardInfo = [
   {
@@ -76,40 +67,8 @@ const cardInfo = [
   },
 ];
 
-export const ProductList = () => {
-  const history = useHistory();
+export const ProductDetail = () => {
+  const { productId } = useParams();
 
-  const handleCardClick = (index) => {
-    history.push(`/product/${index}`);
-  };
-
-  return (
-    <div>
-      <div className="container w-full md:w-3/4 gap-8 flex flex-col items-center justify-center md:justify-between md:flex-row flex-wrap">
-        {cardInfo.map((card, index) => (
-          <Card key={index} className="border-none md:w-1/4 max-w-[239px] ">
-            <CardContent className="flex justify-center w-full">
-              <img
-                className="w-full max-h-[427px] w-1/4 "
-                src={card.imageLink}
-                onClick={() => handleCardClick(index)}
-              />
-            </CardContent>
-            <div className="flex justify-center ">
-              <CardFooter>
-                <div className="flex flex-col items-center">
-                  <p>{card.footerTitle}</p>
-                  <p>{card.footerDescription}</p>
-                  <p>
-                    <span>{card.originalPrice}</span> {card.discountedPrice}
-                  </p>
-                  <img src={card.footerImg} alt="" />
-                </div>
-              </CardFooter>
-            </div>
-          </Card>
-        ))}
-      </div>
-    </div>
-  );
+  return <div className="container mx-auto p-4">hahahaahah{productId}</div>;
 };
