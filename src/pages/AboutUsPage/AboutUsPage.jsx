061edaedd8b01599d8c126bb2cfb9187e5d30919
cansucebesoy <cansucebesoy3@gmail.com>
2024-09-02 16:34:36 +0300

    #7 add video

--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -1,6 +1,7 @@
 import { Content } from "@/components/AboutUs/Content/Content";
 import { Hero } from "@/components/AboutUs/Hero/Hero";
 import { Stats } from "@/components/AboutUs/Stats/Stats";
+import { Video } from "@/components/AboutUs/Video/Video";
 import { Clients } from "@/components/Commons/Clients";
 import { Team } from "@/components/Team/Team";
 
@@ -10,6 +11,7 @@ export const AboutUsPage = () => {
       <Hero />
       <Content />
       <Stats />
+      <Video />
       <Team />
       <Clients />
     </>
