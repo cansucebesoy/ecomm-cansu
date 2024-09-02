5064c818a15e913f0a387ece4d50ad74c9a2954e
cansucebesoy <cansucebesoy3@gmail.com>
2024-09-02 15:08:05 +0300

    #7 about hero and content

---
+++ src/components/AboutUs/Content/Content.jsx
@@ -0,0 +1,26 @@
+export const Content = () => {
+  return (
+    <div className="container px-4 py-8 ">
+      <div className="flex flex-col items-endtext-start ">
+        <p className="text-lg font-bold text-secondary text-start text-red-500 mb-4">
+          Problems trying
+        </p>
+        <div className="flex flex-col md:flex-row ">
+          <div className="text-2xl font-medium text-gray-700 mb-8 w-1/2">
+            <p className="font-bold">
+              Met minim Mollie non desert Alamo est <br />
+              sit cliquey dolor do met sent.
+            </p>
+          </div>
+          <div className="w-1/2">
+            <p>
+              Problems trying to resolve the conflict between the two major
+              realms of <br />
+              Classical physics: Newtonian mechanics
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -0,0 +1,11 @@
+import { Content } from "@/components/AboutUs/Content/Content";
+import { Hero } from "@/components/AboutUs/Hero/Hero";
+
+export const AboutUsPage = () => {
+  return (
+    <>
+      <Hero />
+      <Content />
+    </>
+  );
+};
