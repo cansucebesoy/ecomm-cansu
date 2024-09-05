bc4601673fa4cf376b8d1b9727d33f59e95c37db
cansucebesoy <cansucebesoy3@gmail.com>
2024-09-05 17:37:55 +0300

    #9 fix import

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { clientReducer } from "./ClientSlice/ClientSlice";
-import { productReducer } from "./ProductSlice/ProductSlice";
-import { shoppingCartReducer } from "./ShoppingCartSlice/ShoppingCartSlice";
+import clientReducer from "./ClientSlice/ClientSlice";
+import productReducer from "./ProductSlice/ProductSlice";
+import shoppingCartReducer from "./ShoppingCartSlice/ShoppingCartSlice";
 
 export const store = configureStore({
   reducer: {
