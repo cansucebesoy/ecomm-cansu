792670a7516fdb51b8ec84f42020f29964929d4d
cansucebesoy <cansucebesoy3@gmail.com>
2024-09-10 19:11:17 +0300

    #11 init

---
+++ src/services/axiosInterceptors/axiosInterceptors.js
@@ -0,0 +1,12 @@
+const requestInterceptors = [
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = token;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+];
+
+export { requestInterceptors };
